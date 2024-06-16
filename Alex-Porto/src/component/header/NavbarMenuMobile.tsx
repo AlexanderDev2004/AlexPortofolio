e8445084284ab7c348e73817ad13f0bc081a2904
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-06-16 23:36:20 +0700

    utak-utik

--- Alex-Porto/src/component/header/NavbarMenuMobile.tsx
+++ Alex-Porto/src/component/header/NavbarMenuMobile.tsx
@@ -6,7 +6,7 @@ interface NavbarMenuProps {
 
 export default function NavbarMenuMobile({ hover }: NavbarMenuProps) {
     return (
-        <div className="sm:hidden relative">
+        <div className="lg:hidden relative">
             <div className="dropdown">
                 <button tabIndex={0} className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -15,13 +15,13 @@ export default function NavbarMenuMobile({ hover }: NavbarMenuProps) {
                 </button>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-10 right-0 p-3 shadow bg-base-100 rounded-box w-52">
                     <li>
-                        <Link href="/experiences" className={hover}>
-                            Experiences
+                        <Link href="/experiences">
+                            <a className={hover}>Experiences</a>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/projects" className={hover}>
-                            Projects
+                        <Link href="/projects">
+                            <a className={hover}>Projects</a>
                         </Link>
                     </li>
                 </ul>
