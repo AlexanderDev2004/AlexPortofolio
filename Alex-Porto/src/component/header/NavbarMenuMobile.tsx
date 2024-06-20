db4d1a8346f387ae07bdc8f074388986b733d0bf
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-06-20 15:00:01 +0700

    :{

--- Alex-Porto/src/component/header/NavbarMenuMobile.tsx
+++ Alex-Porto/src/component/header/NavbarMenuMobile.tsx
@@ -9,11 +9,25 @@ export default function NavbarMenuMobile({ hover }: NavbarMenuProps) {
     <div className="lg:hidden relative">
       <div className="dropdown">
         <button tabIndex={0} className="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
           </svg>
         </button>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-10 right-0 p-3 shadow bg-base-100 rounded-box w-52">
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content mt-3 z-10 right-0 p-3 shadow bg-base-100 rounded-box w-52"
+        >
           <li>
             <Link href="/experiences" className={hover}>
               Experiences
