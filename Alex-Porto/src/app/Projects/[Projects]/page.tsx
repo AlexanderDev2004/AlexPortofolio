ef5e0628513229f2c209fc3d66457988dcc2484d
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-09-17 14:55:43 +0700

    Fix error logic

--- Alex-Porto/src/app/Projects/[Projects]/page.tsx
+++ Alex-Porto/src/app/Projects/[Projects]/page.tsx
@@ -12,11 +12,7 @@ interface ProjectDetailPageProps {
 export default function ProjectDetailPage({ params }: ProjectDetailPageProps) {
   const hover =
     "hover:text-primary transition ease-in-out delay-150 hover:-translate-y-0.5 duration-300";
-    
-  // Correctly parse the project ID from the string
   const projectId = parseInt(params.projects); 
-
-  // Find the corresponding project by ID
   const project = projectData.find((item) => item.id === projectId);
 
   return (
@@ -25,10 +21,10 @@ export default function ProjectDetailPage({ params }: ProjectDetailPageProps) {
       <div className="mx-2">
         <h1 className="font-bold text-xl mb-2">Detail Project</h1>
         {project ? (
-          // Pass project data if found
+          
           <ProjectDetail project={project as ProjectDetailProps["project"]} />
         ) : (
-          // Show error message if no project is found
+          
           <p className="text-red-500">Project not found</p>
         )}
       </div>
